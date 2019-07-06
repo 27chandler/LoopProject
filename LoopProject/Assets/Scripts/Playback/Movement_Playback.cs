d981355e5b1db27179e62d405260af54035e2954
27chandler <chandler27s@yahoo.com>
2019-07-06 22:02:41 +0100

    Added player movement, recording and playback

---
+++ LoopProject/Assets/Scripts/Playback/Movement_Playback.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Movement_Playback : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float delay;
+
+    private float current_time = 0.0f;
+    private int timestamp_index = 0;
+    private Vector3 last_position = new Vector3();
+    [SerializeField] private List<Position_Data> position_buffer = new List<Position_Data>();
+
+    [Serializable] public struct Position_Data
+    {
+        public Vector3 position;
+        public float timestamp;
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Add_To_Buffer(target.position, current_time);
+        Add_To_Buffer(target.position, current_time);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        current_time += Time.deltaTime;
+
+        if ((position_buffer[position_buffer.Count-1].position != target.position))
+        {
+            Add_To_Buffer(target.position,current_time);
+        }
+
+        if (position_buffer[timestamp_index+1].timestamp <= current_time - delay)
+        {
+            timestamp_index++;
+            transform.position = position_buffer[timestamp_index].position;
+        }
+    }
+
+    void Add_To_Buffer(Vector3 i_pos, float i_time)
+    {
+        Position_Data input_data = new Position_Data();
+        input_data.position = i_pos;
+        input_data.timestamp = i_time;
+
+        position_buffer.Add(input_data);
+    }
+}
---
+++ LoopProject/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    private CharacterController cc;
+
+    [SerializeField] private float movement_speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3();
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            movement += transform.right;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            movement -= transform.right;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement += transform.forward;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            movement -= transform.forward;
+        }
+
+        cc.SimpleMove(movement * Time.deltaTime * movement_speed);
+    }
+}
