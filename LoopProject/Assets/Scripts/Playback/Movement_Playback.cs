ea9ab87a39f394227822067b712af48fa7a784d6
27chandler <chandler27s@yahoo.com>
2019-07-07 11:09:24 +0100

    Added visibility check to copy player

--- LoopProject/Assets/Scripts/Playback/Movement_Playback.cs
+++ LoopProject/Assets/Scripts/Playback/Movement_Playback.cs
@@ -8,10 +8,10 @@ public class Movement_Playback : MonoBehaviour
     [SerializeField] public bool is_playing;
     [Space]
     [SerializeField] private Transform target;
-    [SerializeField] private Transform camera_target;
+    [SerializeField] private Transform pivot_target;
 
     [Space]
-    [SerializeField] private Transform this_camera;
+    [SerializeField] private Transform this_pivot;
     [SerializeField] private float delay;
 
     private float current_time = 0.0f;
@@ -29,8 +29,8 @@ public class Movement_Playback : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Add_To_Buffer(target.position, camera_target.localRotation, current_time);
-        Add_To_Buffer(target.position, camera_target.localRotation, current_time);
+        Add_To_Buffer(target.position, pivot_target.localRotation, current_time);
+        Add_To_Buffer(target.position, pivot_target.localRotation, current_time);
     }
 
     // Update is called once per frame
@@ -47,16 +47,16 @@ public class Movement_Playback : MonoBehaviour
     {
         current_time += Time.deltaTime;
 
-        if ((position_buffer[position_buffer.Count - 1].position != target.position) || (position_buffer[position_buffer.Count - 1].view_rotation != camera_target.localRotation))
+        if ((position_buffer[position_buffer.Count - 1].position != target.position) || (position_buffer[position_buffer.Count - 1].view_rotation != pivot_target.localRotation))
         {
-            Add_To_Buffer(target.position, camera_target.localRotation, current_time);
+            Add_To_Buffer(target.position, pivot_target.localRotation, current_time);
         }
 
         if (position_buffer[timestamp_index + 1].timestamp <= current_time - delay)
         {
             timestamp_index++;
             transform.position = position_buffer[timestamp_index].position;
-            this_camera.localRotation = position_buffer[timestamp_index].view_rotation;
+            this_pivot.localRotation = position_buffer[timestamp_index].view_rotation;
         }
     }
 
