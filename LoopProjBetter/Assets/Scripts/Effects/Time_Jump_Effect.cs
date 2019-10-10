e7cc00b20921bdc9fd6640e29b7abd4ba472a517
27chandler <chandler27s@yahoo.com>
2019-10-10 15:53:50 +0100

    Modified time travel effect and recalculated lighting

--- LoopProjBetter/Assets/Scripts/Effects/Time_Jump_Effect.cs
+++ LoopProjBetter/Assets/Scripts/Effects/Time_Jump_Effect.cs
@@ -20,12 +20,20 @@ public class Time_Jump_Effect : MonoBehaviour
 
     private Camera player_cam;
     private float default_fov;
+
+    private ChromaticAberration ca;
+    private LensDistortion ld;
+    private ColorGrading cg;
     // Start is called before the first frame update
     void Start()
     {
         player_cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
 
         default_fov = player_cam.fieldOfView;
+
+        ca = profile.GetSetting<ChromaticAberration>();
+        ld = profile.GetSetting<LensDistortion>();
+        cg = profile.GetSetting<ColorGrading>();
     }
 
     // Update is called once per frame
@@ -50,8 +58,9 @@ public class Time_Jump_Effect : MonoBehaviour
             curve_value = curve.Evaluate(timer / max_time);
 
             player_cam.fieldOfView = default_fov + (curve_value * max_fov_increase);
-            profile.GetSetting<ChromaticAberration>().intensity.Override(curve_value);
-            profile.GetSetting<LensDistortion>().intensity.Override(curve_value*80.0f);
+            ca.intensity.Override(curve_value*2.0f);
+            ld.intensity.Override(curve_value*80.0f);
+            cg.lift.Override(new Vector4(0.0f, curve_value/3.0f, 0.0f, 0.0f));
 
             if ((timer > max_time) && (!has_triggered_jump_this_cycle))
             {
@@ -60,9 +69,9 @@ public class Time_Jump_Effect : MonoBehaviour
             }
             yield return null;
         }
-        
 
 
+        cg.lift.Override(new Vector4(0.0f, 0.0f, 0.0f, 0.0f));
 
         timer = 0.0f;
         is_routine_running = false;
