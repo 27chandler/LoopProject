58b48120414cc6fcb18d77e83b7b568045ac63e1
27chandler <chandler27s@yahoo.com>
2020-07-01 13:19:36 +0100

    Added new third person movement system

---
+++ LoopProjBetter/Assets/Prototype Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3();
+
+        movement.x = Input.GetAxis("Horizontal");
+        movement.z = Input.GetAxis("Vertical");
+
+        controller.Move(movement * speed * Time.deltaTime);
+    }
+}
