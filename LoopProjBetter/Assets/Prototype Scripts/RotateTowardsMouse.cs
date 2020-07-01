b8851daaf139d139903e5bcb0e7576fff736d36f
27chandler <chandler27s@yahoo.com>
2020-07-01 16:48:04 +0100

    Added script that rotates object to face the mouse world position

---
+++ LoopProjBetter/Assets/Prototype Scripts/RotateTowardsMouse.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTowardsMouse : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider != null)
+            {
+                Vector3 look_pos = hit.point;
+                look_pos.y = transform.position.y; // Make look position height be player height so player isn't looking into ground or sky
+                transform.LookAt(look_pos);
+            }
+        }
+    }
+}
