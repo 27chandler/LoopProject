900e9ee514cc827218725ef4b6af14a257f6a7fb
27chandler <chandler27s@yahoo.com>
2020-07-01 12:13:10 +0100

    Added scoring

---
+++ LoopProjBetter/Assets/Scripts/Misc/ScoreSystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSystem : MonoBehaviour
+{
+    private int score = 0;
+    private Text score_text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score_text = GetComponent<Text>();
+    }
+
+    public void Add_Score(int i_score)
+    {
+        score += i_score;
+    }
+
+    public void Subtract_score(int i_score)
+    {
+        score -= i_score;
+    }
+
+    public void Set_Score(int i_score)
+    {
+        score = i_score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score_text.text = "Score: " + score;
+    }
+}
