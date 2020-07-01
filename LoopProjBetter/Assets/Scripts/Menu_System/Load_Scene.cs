d17cd371d57f441d20463b4a817f8edf7f69b0b3
27chandler <chandler27s@yahoo.com>
2020-07-01 11:59:52 +0100

    Added menu and load scene systems

---
+++ LoopProjBetter/Assets/Scripts/Menu_System/Load_Scene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load_Scene : MonoBehaviour
+{
+    public void Load_Defined_Scene(string i_name)
+    {
+        SceneManager.LoadScene(i_name);
+    }
+}
