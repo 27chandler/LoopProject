922f521bc4c2e340ba31d640fe1a297dacea15ed
27chandler <chandler27s@yahoo.com>
2020-07-01 21:02:06 +0100

    Removed unused function

--- LoopProjBetter/Assets/Prototype Scripts/RotateTowardsMouse.cs
+++ LoopProjBetter/Assets/Prototype Scripts/RotateTowardsMouse.cs
@@ -5,13 +5,7 @@ using UnityEngine;
 public class RotateTowardsMouse : MonoBehaviour
 {
     [SerializeField] private Camera cam;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
